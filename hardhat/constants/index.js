175f844958ce8bb74f75521284b7ad838af99d8b
krishi <krishichawda0@gmail.com>
2022-08-15 09:10:38 +0530

    Added a NFT-Collection

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x929f9164ed1ccF41806E2cddc6B34533164a0fFC";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
